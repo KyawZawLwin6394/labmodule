632bdfad984f206c2170398bcee06d30200a5d1c
Kyaw Zaw Lwin <kyawzawlwin.work.6394@gmail.com>
2023-04-24 10:24:59 +0630

    final

--- app/models/voucher.js
+++ app/models/voucher.js
@@ -14,48 +14,52 @@ let VoucherSchema = new Schema({
         type: Date,
         required: true
     },
-    relatedPatient : {
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'Patients',
-        required:true
+    relatedPatient: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Patients',
+        required: true
     },
-    referDoctor :{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'Doctors',
-        required:true
+    referDoctor: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Doctors',
+        required: true
     },
-    options:{
-      type:String,
-      enum:['Collect','Email']  
+    options: {
+        type: String,
+        enum: ['Collect', 'Email']
     },
     testSelection: [{
-        name:{
-            type:mongoose.Schema.Types.ObjectId,
-            ref:'Services'
+        name: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Services'
         },
-        qty:Number,
-        unitCharge:Number,
-        subCharge:Number
-      }],
+        qty: Number,
+        unitCharge: Number,
+        subCharge: Number,
+        result:{
+            type:String,
+            default:null
+        }
+    }],
     totalCharge: {
-        type:Number,
-        required:true
+        type: Number,
+        required: true
     },
     discount: {
-        type:Number,
-        required:true
+        type: Number,
+        required: true
     },
-    netDiscount:{
-        type:Number,
-        required:true
+    netDiscount: {
+        type: Number,
+        required: true
     },
     pay: {
-        type:Number,
-        required:true
+        type: Number,
+        required: true
     },
-    change:{
-        type:Number,
-        required:true
+    change: {
+        type: Number,
+        required: true
     },
     createdAt: {
         type: Date,
