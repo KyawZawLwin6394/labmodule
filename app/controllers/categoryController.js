480294c8deb73e78613f5507408fae36f15f8128
Kyaw Zaw Lwin <75203193+KyawZawLwin6394@users.noreply.github.com>
2023-05-13 09:54:41 +0630

    final

--- app/controllers/categoryController.js
+++ app/controllers/categoryController.js
@@ -1,31 +1,46 @@
 'use strict';
 const Category = require('../models/category');
+const AccountingList = require('../models/accountingList');
 
 exports.listAllCategories = async (req, res) => {
   try {
-    let result = await Category.find({isDeleted:false});
-    let count = await Category.find({isDeleted:false}).count();
+    let result = await Category.find({ isDeleted: false });
+    let count = await Category.find({ isDeleted: false }).count();
     res.status(200).send({
       success: true,
       count: count,
       data: result
     });
   } catch (error) {
-    return res.status(500).send({ error:true, message:'No Record Found!'});
+    return res.status(500).send({ error: true, message: 'No Record Found!' });
   }
 };
 
 exports.getCategory = async (req, res) => {
-  const result = await Category.find({ _id: req.params.id,isDeleted:false });
+  const result = await Category.find({ _id: req.params.id, isDeleted: false });
   if (!result)
     return res.status(500).json({ error: true, message: 'No Record Found' });
   return res.status(200).send({ success: true, data: result[0] });
 };
 
-exports.createCategory = async (req, res, next) => {
+exports.createCategory = async (req, res) => {
   try {
     const newCategory = new Category(req.body);
     const result = await newCategory.save();
+    if (req.body.createAcc === true) {
+      let accResult = await AccountingList.create(
+        {
+          relatedType: "6423b7c40a7d6362acb770a0", //revenues
+          relatedHeader: "6423b9b30a7d6362acb770d0", //sales
+          subHeader: req.body.name + ' income',
+          amount: req.body.amount,
+          openingBalance: req.body.amount,
+          carryForWork: false,
+          generalFlag: false
+        }
+      )
+      console.log(accResult)
+    }
     res.status(200).send({
       message: 'Category create success',
       success: true,
