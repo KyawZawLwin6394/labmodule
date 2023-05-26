25b2d51aa041cd39e09e209950f76ec38e96af01
Kaung Set Hein <127381301+kaung-set107@users.noreply.github.com>
2023-05-26 15:43:32 +0630

    update

--- app/routes/accountingRoutes.js
+++ app/routes/accountingRoutes.js
@@ -1,19 +1,25 @@
-"use strict";
+'use strict'
 
-const accountingList = require("../controllers/accountingListController");
-const { catchError } = require("../lib/errorHandler");
-const verifyToken = require('../lib/verifyToken');
+const accountingList = require('../controllers/accountingListController')
+const { catchError } = require('../lib/errorHandler')
+const verifyToken = require('../lib/verifyToken')
 
-module.exports = (app) => {
+module.exports = app => {
+  app
+    .route('/api/accounting-list')
+    .post(catchError(accountingList.createAccountingList))
+    .put(catchError(accountingList.updateAccountingList))
 
-    app.route('/api/accounting-list')
-        .post(catchError(accountingList.createAccountingList))
-        .put(catchError(accountingList.updateAccountingList))
-        
-    app.route('/api/accounting-list/:id')
-        .get(catchError(accountingList.getAccountingList))
-        .delete(catchError(accountingList.deleteAccountingList)) 
-        .post(catchError(accountingList.activateAccountingList))
+  app
+    .route('/api/accounting-list/:id')
+    .get(catchError(accountingList.getAccountingList))
+    .delete(catchError(accountingList.deleteAccountingList))
+    .post(catchError(accountingList.activateAccountingList))
 
-    app.route('/api/accounting-lists').get( catchError(accountingList.listAllAccountingLists))
-};
+  app
+    .route('/api/accounting-lists')
+    .get(catchError(accountingList.listAllAccountingLists))
+  app
+    .route('/api/accounting-lists/related/:id')
+    .get(catchError(accountingList.getRelatedAccount))
+}
